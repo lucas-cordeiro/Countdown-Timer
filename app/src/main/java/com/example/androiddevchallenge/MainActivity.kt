5019d1c1db3cf0a704c78427e714fca8eeb5862d
Lucas Cordeiro <lucas.cordeiro5@etec.sp.gov.br>
2021-03-07 14:18:29 -0300

    feat: NumberOne, NumberTwo and NumberThree

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,12 +18,45 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.math.exp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,13 +69,248 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-// Start building your app here!
+// Start building your app here! 1
 @Composable
 fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+
+        var expanded by remember{ mutableStateOf(true) }
+        var number by remember{ mutableStateOf(1) }
+
+        val scope = rememberCoroutineScope()
+
+        Column() {
+            Text(
+                text = "Ready... Set... GO!",
+                style = MaterialTheme.typography.body1.copy(
+                    fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier
+                    .padding(top = 10.dp, start = 100.dp)
+            )
+
+            when(number){
+                1 -> NumberOne(expanded = expanded, modifier = Modifier
+                    .size(50.dp)
+                    .align(Alignment.CenterHorizontally))
+
+                2 -> NumberTwo(expanded = expanded, modifier = Modifier
+                    .size(50.dp)
+                    .align(Alignment.CenterHorizontally))
+
+                3 -> NumberThree(expanded = expanded, modifier = Modifier
+                    .size(50.dp)
+                    .align(Alignment.CenterHorizontally))
+
+                else -> {}
+            }
+
+           Row() {
+               Button(onClick = {
+                   scope.launch {
+                       expanded = false
+                       delay(ANIMATION_DURATION.toLong())
+                       number -= 1
+                       delay(ANIMATION_DURATION.toLong())
+                       expanded = true
+                   }
+               }) {
+                   Text(text = "-")
+               }
+
+               Button(onClick = {
+                  scope.launch {
+                      expanded = false
+                      delay(ANIMATION_DURATION.toLong())
+                      number += 1
+                      delay(ANIMATION_DURATION.toLong())
+                      expanded = true
+                  }
+               }) {
+                   Text(text = "+")
+               }
+           }
+        }
     }
 }
+
+@Composable
+private fun NumberOne(
+    expanded: Boolean,
+    modifier: Modifier = Modifier
+) {
+    val animSpec: AnimationSpec<Float> = remember {
+        tween(
+            durationMillis = ANIMATION_DURATION,
+        )
+    }
+    val value = remember { Animatable(0f) }
+
+    val path1 = Path()
+    val path2 = Path()
+
+
+    path1.moveTo(value.value, 2.5f)
+    path1.lineTo(0f, 30f)
+
+    path2.moveTo(value.value, 0f)
+    path2.lineTo(value.value, 90f)
+
+
+
+    LaunchedEffect(expanded) {
+        value.animateTo(
+            targetValue = if (expanded) 20f else 0f,
+            animationSpec = animSpec
+        )
+    }
+
+    Canvas(modifier = modifier) {
+        drawPath(
+            path = path1,
+            color = Color.Red,
+            style = Stroke(10f)
+        )
+        drawPath(
+            path = path2,
+            color = Color.Red,
+            style = Stroke(10f)
+        )
+    }
+}
+
+@Composable
+private fun NumberTwo(
+    expanded: Boolean,
+    modifier: Modifier = Modifier
+){
+    val animSpec: AnimationSpec<Float> = remember {
+        tween(
+            durationMillis = ANIMATION_DURATION,
+        )
+    }
+    val value = remember { Animatable(90f) }
+    val value2 = remember { Animatable(0f) }
+    val value3 = remember { Animatable(0f) }
+    val value4 = remember { Animatable(0f) }
+    val value5 = remember { Animatable(0f) }
+
+
+    val path = Path()
+    val path2 = Path()
+    val path3 = Path()
+
+    path.addArc(Rect(0f, 0f, 45f, 45f), 180f, value3.value)
+    path.lineTo(value2.value, value.value)
+
+    path2.moveTo(0f, 85f)
+    path2.lineTo(value4.value, 85f)
+
+    path3.moveTo(0f, 22.5f)
+    path3.lineTo(0f,value5.value)
+
+
+
+    LaunchedEffect(expanded){
+        launch {
+            value.animateTo(
+                targetValue = if(expanded) 85f else 90f,
+                animationSpec = animSpec
+            )
+        }
+        launch {
+            value2.animateTo(
+                targetValue = if(expanded) 4f else 0f,
+                animationSpec = animSpec
+            )
+        }
+        launch {
+            value3.animateTo(
+                targetValue = if(expanded) 200f else 0f,
+                animationSpec = animSpec
+            )
+        }
+        launch {
+            value4.animateTo(
+                targetValue = if(expanded) 50f else 0f,
+                animationSpec = animSpec
+            )
+        }
+        launch {
+            value5.animateTo(
+                targetValue = if(expanded) 22.5f else 0f,
+                animationSpec = animSpec
+            )
+        }
+    }
+
+    Canvas(modifier = modifier){
+        drawPath(
+            path = path,
+            color = Color.Red,
+            style = Stroke(10f)
+        )
+        drawPath(
+            path = path2,
+            color = Color.Red,
+            style = Stroke(10f)
+        )
+        drawPath(
+            path = path3,
+            color = Color.Red,
+            style = Stroke(10f)
+        )
+    }
+}
+
+@Composable
+private fun NumberThree(
+    expanded: Boolean,
+    modifier: Modifier = Modifier
+){
+    val animSpec: AnimationSpec<Float> = remember {
+        tween(
+            durationMillis = ANIMATION_DURATION,
+        )
+    }
+
+    val value = remember { Animatable(0f) }
+    val value2 = remember { Animatable(90f) }
+
+    val path = Path()
+
+    path.lineTo(0f, value2.value)
+    path.moveTo(0f,0f)
+    path.addArc(Rect(0f, 0f, 45f, 45f), 180f, value.value)
+    path.addArc(Rect(0f, 45f, 45f, 90f), 270f, value.value)
+
+    LaunchedEffect(expanded){
+        launch {
+            value.animateTo(
+                targetValue = if(expanded) 280f else 0f,
+                animationSpec = animSpec
+            )
+        }
+        launch {
+            value2.animateTo(
+                targetValue = if(expanded) 0f else 90f,
+                animationSpec = animSpec
+            )
+        }
+    }
+
+    Canvas(modifier = modifier) {
+        drawPath(
+            path = path,
+            color = Color.Red,
+            style = Stroke(10f)
+        )
+    }
+}
+
+
+
+const val ANIMATION_DURATION = 500
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
